db3b9a7701acffd4953e9b93a8e2b22417ad1edf
eric-mokuma <mokumaeric@gmail.com>
2025-06-02 10:39:13 +1200

    navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,32 @@ const App = () => {
   // The header contains a logo and a navigation bar with several links.
   return (
     <header className="header">
-      <a href="/images/favicon-square.png" className="logo">
+      <div className="logo">
         <img
-          src="/images/favicon-square.png"
-          alt="Logo"
-          style={{ height: '74px', width: 'auto', objectFit: 'contain' }}
+          // The logo is an image with a specific source, alt text, and styles for width, height, and object fit.
+          src="/images/logo-and-favicon-modified.png"
+          alt="ARMSS Logo"
         />
-      </a>
+      </div>
 
       <nav className="navbar">
-        <a href="/">Home</a>
-        <a href="/">News & Announcements</a>
-        <a href="/">About us</a>
-        <a href="/">Programs</a>
-        <a href="/">The Team</a>
-        <a href="/">Testimonials</a>
-        <a href="/">Our Poem</a>
-        <a href="/">Contact Us</a>
+        <a href="#" className="home">
+          Home
+        </a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">News & Announcements</a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">About us</a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">Programs</a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">The Team</a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">Testimonials</a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">Our Poem</a>
+        <span style={{ color: 'green' }}>|</span>
+        <a href="#">Contact us</a>
       </nav>
     </header>
   )
